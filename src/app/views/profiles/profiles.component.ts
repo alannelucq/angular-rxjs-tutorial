3bf80be1e5e6846758f5833c79db1d2af5c765f8
arthur <a.lannelucq@gmail.com>
2023-04-22 12:03:55 +0200

    feat: add filter on name

--- src/app/views/profiles/profiles.component.ts
+++ src/app/views/profiles/profiles.component.ts
@@ -3,18 +3,31 @@ import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
 import { Profile } from "../../core/models/profile.model";
 import { ProfilesGateway } from "../../core/gateways/profiles.gateway";
-import { Observable } from "rxjs";
+import { combineLatest, map, Observable, startWith } from "rxjs";
+import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: 'app-profiles',
   standalone: true,
-  imports: [CommonModule, MatSlideToggleModule],
+  imports: [CommonModule, MatSlideToggleModule, ReactiveFormsModule],
   templateUrl: './profiles.component.html',
   styleUrls: ['./profiles.component.scss']
 })
 export class ProfilesComponent {
-  profiles$: Observable<Profile[]> = this.profilesGateway.fetchProfiles();
+  search = this.fb.nonNullable.group({
+    name: ['']
+  });
+  profiles$: Observable<Profile[]> = this.getProfiles();
 
-  constructor(private profilesGateway: ProfilesGateway) {
+  constructor(private profilesGateway: ProfilesGateway, private fb: FormBuilder) {
+  }
+
+  private getProfiles(): Observable<Profile[]> {
+    const profiles$ = this.profilesGateway.fetchProfiles();
+    const searchName$ = this.search.controls.name.valueChanges.pipe(startWith(''));
+    return combineLatest([profiles$, searchName$])
+      .pipe(
+        map(([profiles, name]) => profiles.filter(profile => profile.name.toLowerCase().includes(name.toLowerCase())))
+      )
   }
 }
