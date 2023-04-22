9e678b7f7a088524225e0844bfa615a2146026b1
arthur <a.lannelucq@gmail.com>
2023-04-22 10:52:23 +0200

    fix: fetch profiles from backend

--- src/app/core/gateways/profiles.gateway.ts
+++ src/app/core/gateways/profiles.gateway.ts
@@ -1,7 +1,17 @@
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from "rxjs";
+import { Profile } from "../models/profile.model";
 
 @Injectable({
   providedIn: "root"
 })
 export class ProfilesGateway {
+
+  constructor(private http: HttpClient) {
+  }
+
+  fetchProfiles(): Observable<Profile[]> {
+    return this.http.get<Profile[]>("http://localhost:3000/profiles");
+  }
 }
