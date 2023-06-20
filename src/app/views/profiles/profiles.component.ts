713b93043cb01e4a3ccf0f7802090aafab93e423
arthur <a.lannelucq@gmail.com>
2023-06-20 10:32:28 +0200

    refactor: migrate from observables to signals

--- src/app/views/profiles/profiles.component.ts
+++ src/app/views/profiles/profiles.component.ts
@@ -1,46 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-import { Profile } from "../../core/models/profile.model";
 import { ProfilesGateway } from "../../core/gateways/profiles.gateway";
-import { combineLatest, Observable, startWith, switchMap } from "rxjs";
-import { FormBuilder, ReactiveFormsModule } from "@angular/forms";
+import { debounceTime, switchMap } from "rxjs";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { toObservable, toSignal } from "@angular/core/rxjs-interop";
 
 @Component({
   selector: 'app-profiles',
   standalone: true,
-  imports: [CommonModule, MatSlideToggleModule, ReactiveFormsModule],
+  imports: [CommonModule, MatSlideToggleModule, ReactiveFormsModule, FormsModule],
   templateUrl: './profiles.component.html',
   styleUrls: ['./profiles.component.scss']
 })
 export class ProfilesComponent {
-  search = this.fb.nonNullable.group({
-    name: [''],
-    job: [''],
-    location: [''],
-    available: [false]
-  });
-  profiles$: Observable<Profile[]> = this.getProfiles();
+  profilesGateway = inject(ProfilesGateway);
 
-  constructor(private profilesGateway: ProfilesGateway, private fb: FormBuilder) {
-  }
-
-  private getProfiles(): Observable<Profile[]> {
-    const search$ = combineLatest([
-      this.search.controls.name.valueChanges.pipe(startWith('')),
-      this.search.controls.job.valueChanges.pipe(startWith('')),
-      this.search.controls.location.valueChanges.pipe(startWith('')),
-      this.search.controls.available.valueChanges.pipe(startWith(false))
-    ]);
-
-    return search$
-      .pipe(
-        switchMap(([name, job, location, availableOnly]) => this.profilesGateway.fetchProfiles({
-          name,
-          job,
-          location,
-          availableOnly
-        }))
-      );
-  }
+  name = signal('');
+  job = signal('');
+  location = signal('');
+  available = signal(false);
+  search = computed(() => ({
+    name: this.name(),
+    job: this.job(),
+    location: this.location(),
+    availableOnly: this.available()
+  }));
+  profiles = toSignal(
+    toObservable(this.search).pipe(
+      debounceTime(200),
+      switchMap(search => this.profilesGateway.fetchProfiles(search))
+    )
+  );
 }
